3f1b7827b752da693b71e762075e3792549c0a97
Vondia <pimvdbosch@gmail.com>
2022-07-08 17:18:56 +0200

    Added new components to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Navbar } from "../components/NavBar";
+import { About } from "../components/About";
+import { Main } from "../components/Main";
+import { Navbar } from "../components/navbar/NavBar";
+import { Skills } from "../components/skills";
 import { Heading } from "../components/ui/typograhpy/Heading";
 
 const Home: NextPage = () => {
@@ -10,8 +13,13 @@ const Home: NextPage = () => {
         <title>Pim | Front-End Developer</title>
       </Head>
       <Navbar />
+      <Main />
+      <About />
+      <Skills />
     </>
   );
 };
+
+// 1:05 minuten --> https://www.youtube.com/watch?v=CMx51wpd7g4&t=362s
 
 export default Home;
